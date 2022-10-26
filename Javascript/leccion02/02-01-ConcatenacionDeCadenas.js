940dddfd6d890dc526fbf19199ffb3d3d0eb8d5d
Renzo Viscio <nviscio@gmail.com>
2022-10-25 22:00:56 -0300

    Commit Clase 7
    Ejercicios par impar, mayor o menor

--- Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -11,5 +11,24 @@ console.log(juntos);
 juntos = 78 + 17 + nombre; //Colocandolos al principio la consola toma los números como enteros
 console.log(juntos);
 
-nombre += apellido;
+nombre += apellido; //Concatenación usando el operador simplificado
 console.log(nombre);
+
+//Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Perez"; una constante no puede ser modificada
+console.log(apellido2);
+
+let x,y; //Se pueden crear varias variables dentro de una misma línea
+x = 17, y = 21 //Se pueden asignar múltiples variables dentro de la misma línea
+let z = x + y; //Se puede asignar el valor de la operación
+console.log(z);
+
+let _1num = 31; //No utilizar números para iniciar el nombre de una variable
+let rompiendo = "rompe"; //No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
---
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar números pares
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un número par");
+}
+else{
+    console.log("Es un número impar");
+}
+
+//Ejercicio mayor de edad
+let edad = 18;
+if(edad >= 18){
+    console.log("Es mayor de edad");
+}
+else{
+    console.log("Es menor de edad");
+}
