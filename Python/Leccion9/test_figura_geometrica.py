cce69d35629b7b426717ccb4067c60091ac36ffd
Renzo <nviscio@gmail.com>
2022-11-01 22:16:45 -0300

    Laboratorio II Clase 12
    POO Parte 5 Herencia

---
+++ Python/Leccion9/test_figura_geometrica.py
@@ -0,0 +1,16 @@
+from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
+
+cuadrado1 = Cuadrado(5, 'Azul')
+print(cuadrado1.ancho)
+print(cuadrado1.alto)
+print(f'Cálculo del área del cuadrado: {cuadrado1.calcular_area()}')
+
+# MRO = Method Resolution Order
+print(Cuadrado.mro())
+
+print(cuadrado1)
+
+rectangulo1 = Rectangulo(3, 8, 'Verde')
+print(f'Calculo del área del rectángulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
